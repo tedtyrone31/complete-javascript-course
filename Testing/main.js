bd7f1301255aa924483d3fe7e9b4ab91de168d78
DESKTOP-3JCH8C5\PC101 <tedtyrone31@yahoo.com>
2022-04-17 01:05:05 +0800

    test

--- Testing/main.js
+++ Testing/main.js
@@ -1,26 +1,68 @@
 "strict mode";
 
+const model = [];
+
 $(document).ready(function () {
-  const test = $("#test");
-  const test2 = $("#test2");
-  const list = $(".list");
-  const ul = $("ul");
-  const list3 = $(".list3");
+  const language = $("#language");
 
-  console.log(ul);
-  $(".button").click(function () {
-    $("body").toggleClass("black");
-    $("#dark").toggleClass("hidden");
-    $("#light").toggleClass("hidden");
-    $("h1").toggleClass("white");
-  });
+  language.on("change", function () {
+    const languageSelected = this.value;
+    console.log(languageSelected);
 
-  $(".list3").click(function () {
-    $(".color").toggleClass("hidden");
+    if (languageSelected == "fruit") {
+      $("#language2").html(`
+      <option value="apple">Apple</option>
+      <option value="banana">Banana</option>
+      <option value="orange">Orange</option>
+    </select>
+    `);
+    }
   });
-  $(".btn").click(function () {
-    $(".btn").toggleClass("hidden");
-    $(".btn").toggleClass("hideNav");
-    $(".myNav").toggleClass("open");
-  });
+});
+
+let listItem = $("#list_item");
+const phoneList = $("#phoneList");
+const addList = $("#btnAddList");
+const del = $("#btnDel");
+const hideMe = $(".hideMe");
+const body = $("#body");
+const unset = $("#btnUnset");
+
+addList.on("click", function () {
+  if (listItem.val() == 0) {
+    alert("Please input something");
+  } else {
+    phoneList.append(`<li>${listItem.val()}</li>`);
+    model.push(listItem.val());
+    listItem.val("");
+    console.log(model);
+    hideMe.show();
+  }
+});
+
+del.on("click", function () {
+  console.log(model);
+  model.pop(listItem.val());
+  $("li:last").remove();
+  hideMe.hide();
+});
+
+const customDisplay = $("#customDisplay");
+const btnCustomize = $("#customize");
+const btnUnset = $("#btnUnset");
+
+btnCustomize.on("click", function () {
+  // $("li").slideToggle();
+  // customDisplay.css("fontSize", "50px");
+  // customDisplay
+  //   .css("color", "red")
+  //   .animate({ fontSize: "52px", letterSpacing: "100px" });
+});
+
+unset.on("click", function () {
+  // customDisplay
+  //   .css("font-size", "32px")
+  //   .animate({ color: "black", letterSpacing: "0" });
+  // customDisplay.css("color", "black").slideToggle(500);
+  customDisplay.toggleClass("green");
 });
